5e27b533cc341915eaf8bc6049a1229586d6ff13
chnbohwr <chnbohwr@gmail.com>
2017-06-22 22:18:37 +0800

    html and css

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -6,7 +6,31 @@ export default class Home extends Component {
   render() {
     return (
       <div id="pageHome">
-        this is home page
+        <div className="item item1">01</div>
+        <div className="item item2">02</div>
+        <div className="item item3">03</div>
+        <div className="item item4">04</div>
+        <div className="item item5">05</div>
+        <div className="item item6">06</div>
+        <div className="item item7">07</div>
+        <div className="item item8">08</div>
+        <div className="item item9">09</div>
+        <div className="item item10">10</div>
+        <div className="item item11">11</div>
+        <div className="item item12">12</div>
+        <div className="item item13">13</div>
+        <div className="item item14">14</div>
+        <div className="item item15">15</div>
+        <div className="item item16">16</div>
+        <div className="item item17">17</div>
+        <div className="item item18">18</div>
+        <div className="item item19">19</div>
+        <div className="item item20">20</div>
+        <div className="item item21">21</div>
+        <div className="item item22">22</div>
+        <div className="item item23">23</div>
+        <div className="item item24">24</div>
+        <div className="item item25">25</div>
       </div>
     );
   }
