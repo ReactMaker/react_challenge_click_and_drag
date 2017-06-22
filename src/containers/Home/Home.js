ecf54ba7dfbdcc7bcc9bb58a7ba206e9e17afd6f
chnbohwr <chnbohwr@gmail.com>
2017-06-22 21:47:22 +0800

    first commit

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './Home.less';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div id="pageHome">
+        this is home page
+      </div>
+    );
+  }
+}
